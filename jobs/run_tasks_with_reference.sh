522768a7d892581accc7e7368ee3891406f172cf
danielward27 <danielward27@outlook.com>
2024-06-10 19:08:28 +0100

    Update job to lastest version

--- jobs/run_tasks_with_reference.sh
+++ jobs/run_tasks_with_reference.sh
@@ -4,13 +4,20 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem=8GB
-#SBATCH --time=4:00:00
+#SBATCH --time=2:00:00
 #SBATCH --account=math026082
 #SBATCH --array=0-20
 
 # Example submission: sbatch jobs/run_tasks_with_reference.sh
 module load lang/python/miniconda/3.9.7
-source activate flowjax_env
+source activate softce_env
+
+# Note contructing a compatible environment, have both softce and softce_validation in your current directory and run:
+# conda create --name softce_env python
+# conda activate softce_env
+# pip install -e softce
+# pip install -e softce_validation
+
 
 task_names=("eight_schools" "multimodal_gaussian_flexible" "multimodal_gaussian_inflexible" "slcp" "linear_regression")
 
