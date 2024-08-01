dcf9e1bcf375872e01a62c9884dca687a8268f2c
danielward27 <danielward27@outlook.com>
2024-08-01 16:25:48 +0100

    Remove unwanted task

--- softcvi_validation/tasks/available_tasks.py
+++ softcvi_validation/tasks/available_tasks.py
@@ -1,3 +1,5 @@
+"""Module contains a utility for getting the available tasks."""
+
 from softcvi_validation.tasks.eight_schools import EightSchoolsTask
 from softcvi_validation.tasks.garch import GARCHTask
 from softcvi_validation.tasks.linear_regression import LinearRegressionTask
@@ -5,6 +7,7 @@ from softcvi_validation.tasks.slcp import SLCPTask
 
 
 def get_available_tasks():
+    """Get a list of the available tasks."""
     task_list = [
         GARCHTask,
         EightSchoolsTask,
@@ -12,7 +15,3 @@ def get_available_tasks():
         SLCPTask,
     ]
     return {t.name: t for t in task_list}
-
-
-# For now we omit MultimodelGaussianFlexibleTask and MultimodelGaussianInflexibleTask,
-# they are very similar to a simpler version of SLCP, so we'll use that instead.
