c061f1b007b0e1cc67b4b4c366c484cb9e2d859c
danielward27 <danielward27@outlook.com>
2024-08-01 16:26:18 +0100

    update jobs

--- jobs/submitter.sh
+++ jobs/submitter.sh
@@ -6,12 +6,12 @@
 declare -A tasks
 tasks=(
   ["garch"]="01:30:00"
-  ["eight_schools"]="01:30:00"
-  ["linear_regression"]="01:30:00"
-  ["slcp"]="02:00:00"
+  ["eight_schools"]="01:15:00"
+  ["linear_regression"]="00:45:00"
+  ["slcp"]="01:30:00"
 )
 
-distributions=("posterior" "proposal")
+distributions=("proposal" "posterior")
 
 for task in "${!tasks[@]}"; do
   run_time=${tasks[$task]}
